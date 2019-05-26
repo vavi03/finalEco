eb1b8a20c58cb79bb524bce08439d70d07961662
vavi03 <vrt0397@gmail.com>
2019-05-25 22:02:25 -0500

    juego 1 hecho papaaaa

---
+++ main.js
@@ -0,0 +1,15 @@
+//juego 1
+
+var juego1img= document.querySelector('.juego');
+var juego1preguntas= document.querySelector('.preguntas');
+
+function cambiarPantallaJuego1(){
+    var t= setTimeout("juego1cambio()",3000);
+}
+cambiarPantallaJuego1();
+
+function juego1cambio(){
+    juego1img.style.display= "none";
+   console.log("cambie");
+   juego1preguntas.style.display= "flex";
+}
