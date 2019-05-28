cb9f5ff89fb3b2663960b5a984584375c2a7d888
vavi03 <vrt0397@gmail.com>
2019-05-28 12:59:49 -0500

    mcdsñ

--- scripts/juego1js.js
+++ scripts/juego1js.js
@@ -7,29 +7,26 @@ var firebaseConfig = {
     storageBucket: "neuron-9174e.appspot.com",
     messagingSenderId: "522583042847",
     appId: "1:522583042847:web:15534444bdf06428"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Get a reference to the database service
-  var database = firebase.database();
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Get a reference to the database service
+var database = firebase.database();
 
-  
+if (userId) {
+    document.querySelectorAll('.pregunta__txt')[0].innerHTML = userId;
+}
 
+var juego1img = document.querySelector('.juego');
+var juego1preguntas = document.querySelector('.preguntas');
 
-var juego1img= document.querySelector('.juego');
-var juego1preguntas= document.querySelector('.preguntas');
-
-function cambiarPantallaJuego(){
-    var t= setTimeout("juegocambio()",3000);
+function cambiarPantallaJuego() {
+    var t = setTimeout("juegocambio()", 3000);
 }
 cambiarPantallaJuego();
 
-function juegocambio(){
-    juego1img.style.display= "none";
-   console.log("cambie");
-   juego1preguntas.style.display= "flex";
+function juegocambio() {
+    juego1img.style.display = "none";
+    console.log("cambie");
+    juego1preguntas.style.display = "flex";
 }
-
-
-
-
