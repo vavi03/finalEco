5749f7b6c890ebb04face54f0fd21abc4809eefa
vavi03 <vrt0397@gmail.com>
2019-05-28 13:15:16 -0500

    css

--- scripts/juego1js.js
+++ scripts/juego1js.js
@@ -13,9 +13,12 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 var database = firebase.database();
 
+//cuando userId es != de null, tiene el uid del usuario
+
+/*
 if (userId) {
     document.querySelectorAll('.pregunta__txt')[0].innerHTML = userId;
-}
+}*/
 
 var juego1img = document.querySelector('.juego');
 var juego1preguntas = document.querySelector('.preguntas');
@@ -30,3 +33,25 @@ function juegocambio() {
     console.log("cambie");
     juego1preguntas.style.display = "flex";
 }
+
+//firebase
+
+function escribirRespuestas(){
+
+    var respuestas = [];
+
+    respuesta[0] = "";
+    respuesta[1] = "";
+    respuesta[2] = "";
+    
+    
+    //firebase
+        
+    //
+
+    location.href = "https://vavi03.github.io/finaEco/juego1.html";
+}
+
+let btnEnviar = document.querySelector("#btnEnviar");
+
+btnEnviar.addEventListener('click', escribirRespuestas);
